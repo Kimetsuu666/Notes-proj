ad66d6a22bd936cc607f142dbe377e295117114b
Yaroslav Levchenko <yl30042004@gmail.com>
2022-06-08 18:43:09 +0300

    Added Formik, Yup, uuid

--- src/notes/hooks/hooks.js
+++ src/notes/hooks/hooks.js
@@ -1,54 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-export function useInputValue(startValue) {
+function useInputValue(startValue) {
   const [value, setValue] = useState(startValue);
 
   const onChange = (e) => {
     setValue(e.target.value);
   };
 
-  const onClear = () => {
-    setValue("");
-  };
-
-  return { value, onChange, onClear };
+  return { value, onChange };
 }
 
-export function useTextValidation(value, validators, isTouched) {
-  const [errorMessage, setErrorMessage] = useState("");
-  const validate = (valueParameter) => {
-    for (const validator of validators) {
-      if (!validator.validator(valueParameter)) {
-        return {
-          isValid: false,
-          errorMessageFromValidator: validator.errorMessage,
-        };
-      }
-    }
-    return { isValid: true };
-  };
-
-  const setError = (valueParameter) => {
-    const { isValid, errorMessageFromValidator } = validate(valueParameter);
-
-    if (isValid) {
-      setErrorMessage("");
-    } else {
-      setErrorMessage(errorMessageFromValidator);
-    }
-  };
-
-  useEffect(() => {
-    if (isTouched) {
-      setError(value);
-    } else {
-      setErrorMessage("");
-    }
-  }, [value, validators, isTouched]);
-
-  return [
-    errorMessage,
-    (valueParameter) => validate(valueParameter || value),
-    (valueParameter) => setError(valueParameter || value),
-  ];
-}
+export default useInputValue;
